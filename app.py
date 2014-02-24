240af69dc7d722206460d2bf11acf5b816473cec
Aloke Desai <aloke.desai@gmail.com>
2014-02-23 23:23:02 -0800

    deleted unnecessary button

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ app.jinja_env.add_extension('jinja2.ext.loopcontrols')
 connection = MongoClient("ds029638.mongolab.com", 29638) 
 db = connection["api"]
 db.authenticate("alokedesai","domino")
-# userdb = client.db
-# user_cols = userdb.cols
+
 
 @app.route('/results', methods=["POST","GET"])
 def results():
@@ -37,37 +36,10 @@ def results():
 	length = len(course_list)
 	if length >= 13:
 		length = 13
-	return render_template('shuffle.html', course_list=course_list, length = length)
+	return render_template('index.html', course_list=course_list, length = length)
 
-# @app.route('/remove', methods=['GET'])
-# def remove():
-# 	userdb.user_cols.remove()
-# 	user_course_list = list(userdb.user_cols.find())
-# 	return render_template('results.html', user_course_list=user_course_list)
-@app.route('/<major>/<lownum>/<highnum>/<school>', methods=["GET"])
-def all(major,lownum,highnum,school):
-	try:
-		lownum2 = int(lownum)
-		highnum2 = int(highnum)
-	except ValueError:
-		lownum2 = 0
-		highnum2 = 1000 
-	if major == "any" and school == "any":
-		course_list = list(db.cols.find( {"number" : { "$gte" : lownum2, "$lte" : highnum2}}))
-	elif major == "any" :
-		course_list = list(db.cols.find( {"number" : { "$gte" : lownum2, "$lte" : highnum2}, "school" : school }))	
-	elif school == "any" :
-		course_list = list(db.cols.find( {"major" : major, "number" : { "$gte" : lownum2, "$lte" : highnum2 } }))	
-	else:
-		course_list = list(db.cols.find( {"major" : major, "number" : { "$gte" : lownum2, "$lte" : highnum2 }, "school" : school }))	
-	length = len(course_list)
-	size = length
-	if length >= 13:
-		length = 13
 
-	random.shuffle(course_list)
 
-	return render_template("shuffle.html", course_list = course_list, length =length, size=size)
 @app.route("/getmore")
 def getmore():
 	course_list = db.cols.find()
@@ -76,11 +48,7 @@ def getmore():
 	print (course_list)
 	print dict(course_list)
 	return "x"
-	
 
-@app.route("/test")
-def test():
-	return render_template("text.html")
 #test for learning ajax
 @app.route('/_add_numbers')
 def add_numbers():
@@ -94,50 +62,10 @@ def add_numbers():
                 mimetype='application/json')
     return resp
 
-	
-	 
-@app.route('/class/setfavorite/<course_id>', methods=['POST','GET'])
-def setFavorite(course_id):
-	#course = list(client.db.cols.find_one({"_id['ObjectId']": class_id}))
-	#return str(course_id)
-	if course_id is not None:
-		db.cols.update({"_id": ObjectId(course_id)},
-		{
-			'$set': { 'favorite': True }
-		}
-		)
-	return str((db.cols.find_one({"_id": ObjectId(course_id)})))
-	# str(client.db.collection_names())
-	
-@app.route('/class/unsetfavorite/<course_id>', methods=['POST','GET'])
-def unsetFavorite(course_id):
-	#course = list(client.db.cols.find_one({"_id['ObjectId']": class_id}))
-	#return str(course_id)
-	if course_id is not None:
-
-		db.cols.update({"_id": ObjectId(course_id)},
-		{
-			'$set': { 'favorite': False }
-		}
-		)
-	return str((db.cols.find_one({"_id": ObjectId(course_id)})))
-	# str(client.db.collection_names())
-
-@app.route('/hong')
-def hong():
-	course_list = list(db.cols.find())
-	random.shuffle(course_list)
-	return render_template('index.html', course_list=course_list)
-
-# @app.route('/')
-# def index():
-# 	course_list = list(db.cols.find())
-# 	random.shuffle(course_list)
-# 	return render_template('index.html', course_list=course_list)
 
 @app.route("/")
 def index():
-	return render_template("shuffle.html")
+	return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
